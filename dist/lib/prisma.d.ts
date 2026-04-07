49e55c7ee8c9901532d2ca280a1f1841a9ad600e
Rupam <rupampw007@gmail.com>
2026-04-07 22:17:26 +0530

    deleted dist folder using git rm -r dist/ command

--- dist/lib/prisma.d.ts
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-declare const prismaClient: import("../src/generated/prisma/internal/class.js").PrismaClient<never, import("../src/generated/prisma/internal/prismaNamespace.js").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
-export { prismaClient };
-//# sourceMappingURL=prisma.d.ts.map
--- dist/lib/prisma.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../src/generated/prisma/client.js";
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-const prismaClient = new PrismaClient({ adapter });
-export { prismaClient };
-//# sourceMappingURL=prisma.js.map
